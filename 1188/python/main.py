7b4a7499e7198a5e15345576e41ccfeeea24547b
Breno1112 <contato.brenofachini@hotmail.com>
2025-02-02 20:59:03 -0300

    done

---
+++ 1188/python/main.py
@@ -0,0 +1,27 @@
+operation = input()
+
+result = None
+count = 0
+
+matrix = []
+
+i = 0
+
+while i < 12:
+    j = 0
+    while j < 12:
+        num = float(input())
+        if j > 11 - i and j < i:
+            if result is None:
+                result = num
+            else:
+                result += num
+            count += 1
+        j += 1
+    i += 1
+
+if operation == 'M':
+    result = result / count
+
+
+print('{:.1f}'.format(result))
